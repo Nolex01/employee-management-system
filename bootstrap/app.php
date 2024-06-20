0007e2a8e54898c710db3d8f6e9427a516acb916
Nolex01 <noel.szabo02@icloud.com>
2024-06-20 17:27:33 +0200

    fix: move admin middleware to the top, site should work now

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,15 +11,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'admin' => App\Http\Middleware\EnsureUserIsAdmin::class
+        ]);
+    })
+    ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-    })
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'admin' => App\Http\Middleware\EnsureUserIsAdmin::class
-        ]);
+
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
