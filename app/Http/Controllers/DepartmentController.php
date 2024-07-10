8189244001933723b161f73a0da1d9e221e78dc4
Nolex01 <noel.szabo02@icloud.com>
2024-07-10 16:24:31 +0200

    feat: implement List function

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Department;
 
 class DepartmentController extends Controller
 {
-    //
+    public function list()
+    {
+        $departments = Department::all();
+
+        return Inertia::render('Admin/Departments/List', [
+            'departments' => $departments,
+        ]);
+    }
 }
