71f79500432bacd335e68738d18decce9f1c32ec
Nolex01 <noel.szabo02@icloud.com>
2024-07-18 19:00:41 +0200

    feat: add error display

--- resources/js/Pages/Admin/Employees/Partials/CreateEmployeeForm.jsx
+++ resources/js/Pages/Admin/Employees/Partials/CreateEmployeeForm.jsx
@@ -154,6 +154,17 @@ const CreateEmployeeForm = ({ departments, roles }) => {
                     />
                 </div>
 
+                {Object.keys(errors).length > 0 && (
+                    <div className="mt-4 p-2 bg-red-100 border border-red-400 text-red-700">
+                        <p className="font-bold">Please fix the following errors:</p>
+                        <ul className="list-disc list-inside">
+                            {Object.keys(errors).map((key) => (
+                                <li key={key}>{errors[key]}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+
                 <div className="flex justify-end">
                     <PrimaryButton disabled={processing}>
                         {processing ? 'Creating...' : 'Create Employee'}
